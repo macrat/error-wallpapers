eef8ecbe0facbfc70812fcb202d12570d97b3e06
MacRat <m@crat.jp>
2018-06-03 22:56:56 +0900

    Made dark theme

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 for size in 1366x768 1440x900 1920x1080 2304x1440 3200x1800 3840x2160; do
-	mkdir ${size}
+	mkdir light-${size}
+	mkdir dark-${size}
 
 	for file in `ls svg/*.svg`; do
-		inkscape --export-background white --export-width ${size%%x*} --export-height ${size##*x} --export-png ${size}/$(basename ${file%.*}).png $file
+		inkscape --export-background=white --export-width=${size%%x*} --export-height=${size##*x} --export-png=light-${size}/$(basename ${file%.*}).png $file
+
+		convert -alpha Remove -negate light-${size}/$(basename ${file%.*}).png dark-${size}/$(basename ${file%.*}).png
 	done
 done
